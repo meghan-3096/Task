fe7c0f8feb7501030278fdd02408a1a02b93c46c
meghan3096 <meghanandi909@gmail.com>
2022-02-09 16:52:21 +0530

    Adding Project Files

--- migration/v1.1.1__firstfile.sql
+++ migration/v1.1.1__firstfile.sql
@@ -1,1 +1,1 @@
-SELECT username, task_description FROM TASK.PUBLIC.TASKDETAILS as t join TASK.PUBLIC.DASHBOARD as d ON (t.task_id = d.task_id) WHERE username='HARRY';
+SELECT username FROM TASK.PUBLIC.TASKDETAILS as t join TASK.PUBLIC.DASHBOARD as d ON (t.task_id = d.task_id) WHERE username='HARRY';
